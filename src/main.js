57ac6393e705f0d8a659d12a46500bf58a149a4f
samghadri <sam.ghadri@yapily.com>
2022-06-29 13:31:30 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import DanManSimple from 'dans-components'
 
 Vue.config.productionTip = false
+Vue.use(DanManSimple);
 
 new Vue({
   render: h => h(App),
